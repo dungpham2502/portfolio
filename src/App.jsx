f3e90e5857e1d3fbff8ec153bf7c1312d3aab43e
Dung Pham <phamanhdung250204@gmail.com>
2023-12-28 21:52:46 -0700

    change

--- src/App.jsx
+++ src/App.jsx
@@ -15,10 +15,9 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='/portfolio' element={<Home />} />
           <Route path='/about' element={<About />} />
           <Route path='/projects' element={<Projects />} />
-          <Route path='/Contact' element={<Contact />} />
+          <Route path='/contact' element={<Contact />} />
         </Routes>
         <Footer />
       </BrowserRouter>
