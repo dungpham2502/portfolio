35d9d83a40437a687ee0f91845849ecdd5a919b9
Dung Pham <phamanhdung250204@gmail.com>
2023-12-28 21:45:39 -0700

    change

--- src/data.js
+++ src/data.js
@@ -9,35 +9,35 @@ export const projects = [{
         id: 2,
         title: 'Task Bulls',
         description: 'An online platform that allows USF students to post and find various tasks and services (MERN)',
-        imgSource: '/portfolio/React.svg',
+        imgSource: 'React.svg',
         link: 'https://github.com/dungpham2502/Task_Bulls'
     },
     {
         id: 3,
         title: 'My Gym',
         description: 'A web app that allow individual users to keep tracks of gym prgression like reps, and sets      (MERN)',
-        imgSource: '/portfolio/React.svg',
+        imgSource: 'React.svg',
         link: 'https://github.com/dungpham2502/mygym'
     },
     {
         id: 4,
         title: 'Personal Portfolio',
         description: 'This portfolio where I show my projects that I did in my free time and ways to connect with me',
-        imgSource: '/portfolio/React.svg',
+        imgSource: 'React.svg',
         link: 'https://github.com/dungpham2502/my_portfolio'
     },
     {
         id: 5,
         title: 'Local Library',
         description: 'A clone library system, where users can browse available books and manage their accounts. (Python/Django, SQL)',
-        imgSource: '/portfolio/Python.svg',
+        imgSource: 'Python.svg',
         link: 'https://github.com/dungpham2502/locallib'
     },
     {
         id: 6,
         title: 'Line-Following Robot',
         description: 'Code for a robot to follow a black line using Arduino and IR sensor (Arduino Programming Language)',
-        imgSource: '/portfolio/Arduino_IDE_logo.svg',
+        imgSource: 'Arduino_IDE_logo.svg',
         link: 'https://github.com/dungpham2502/line-following-robot'
     }
 ]
